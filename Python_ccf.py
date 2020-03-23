329754c18dc1f70ec48b213a5a6b5a79c1d95106
adrian garcia rojas <adrian_gar016@hotmail.es>
2020-03-23 13:51:23 -0600

    Indices y sublistas

--- Python_ccf.py
+++ Python_ccf.py
@@ -1,12 +1,13 @@
-#Que son las listas
+#Indices y sublistas
 
-lista = []
+cursos = ["python", "django", "flask", "c", "c++", "c#", "java", "php"]
+print(cursos[0])
 
-#nombre de la variable, signo igual, corchetes
-
-lista1 = ["Pepe", 23, True, 19.5]
-#La lista almacena string, numero entero, booleano y flotante
-#Con la listas se puede almacenar  N cantidad de estring, booleanos etc 
-# para saber el tamaño de la lista se debe considerar de la sig forma tam -1
-
-print(lista1)
+#sublista
+sub = cursos[0:3]
+sub1 = cursos[1:7:2] #quiero la lista desde la posicion 1 a la 7 con saltos de 2 en dos
+sub2 = cursos[:5] #le indica a python que requerimos los valores desde la posicion 0 a la 5
+sub3 = cursos[2:] #le indica a python que queremos los valores desde la posicion 2 a la ultima
+sub4 = cursos[:] #python copiara toda la lista tal cual esta
+sub5 = cursos[::-1] #regresara el inverso de nuestra lista (inverso)
+print(sub3)
