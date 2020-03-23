22c6bfe56c31108d8a1e3f23381394297998691c
adrian garcia rojas <adrian_gar016@hotmail.es>
2020-03-23 11:36:27 -0600

    Palabras reservadas en Python y enlace para documentacion

--- Python_ccf.py
+++ Python_ccf.py
@@ -1,5 +1,25 @@
-CONSTANTE = "Soy una constante" #Las constantes no existen en python, se tiene que hacer esta declaracion con mayusculas asi otros programadores sabran que son constantes y no deben de modificar su valor
-nombre_tutor = "Adrian Garcia"
+#Palabras reservadas en Python
+#Para conocer el listado ingresar al siguiente link:
+#https://www.programiz.com/python-programming/keyword-list
 
-print(nombre_tutor)
-print(CONSTANTE)
+
+
+"""Las palabras reservadas, como su nombre lo indican, son palabras las cuales el lenguaje de programación ya ha reservado para realizar ciertas tareas.
+
+    Nosotros no podemos declarar una variable con el mismo nombre que una palabra reservada
+
+En python el listado de palabras reservadas es el siguiente:
+			
+False 	class 	is 	return
+None 	continue 	lambda 	try
+True 	def 	nonlocal 	while
+and 	del 	not 	with
+as 	elif 	or 	yield
+assert 	else 	pass 	*
+break 	except 	raise 	*
+
+En el curso trabajaremos con la mayoría de las palabras reservadas, sin embargo, si quieres conocer todas ellas, puedes consultar el siguiente link
+"""
+
+enlace = "https://www.programiz.com/python-programming/keyword-list"
+print("ingresa a la siguinete direccion para conocer todas las palabras reservadas del lenguaje python y mucho mas hay una guia " + enlace)
