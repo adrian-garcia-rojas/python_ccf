881cfddb9c032e66650b090d60ba5434bbd1966c
adrian garcia rojas <adrian_gar016@hotmail.es>
2020-03-23 12:32:13 -0600

    Entrada de datos por teclado

--- Python_ccf.py
+++ Python_ccf.py
@@ -1,23 +1,25 @@
-#Operadores relacionales 6, son simboloes para comparar valores donde optemdremos true false (boleanos)
+#Entrada de datos por teclado
 
-variable_uno = 10
-variable_dos = 18
-
-mayor = variable_uno > variable_dos
-menor = variable_uno < variable_dos
-mayor_igual = variable_uno >= variable_dos
-menor_igual = variable_uno <= variable_dos
-igual = variable_uno == variable_dos
-diferente = variable_uno != variable_dos
-
-comparacion_dos_numeros_enteros = variable_uno is variable_dos
-
-#Operadores logicos 3, and, or, not
-
-resultado = True and True and mayor
-resultado2 = False or False or diferente
-print(resultado)
-print(resultado2)
+#Comentado
+#print("Cual es tu nombre?")
+#nombre = input() #la funcion input() -> regresa un valor de tipo string
+#print("Cual es tu edad?")
+#edad = int(input()) #de esta forma int va a tomar la funcion input() y la convertira en un entero
+#print("cual es tu peso?")
+#peso = float(input())
+#print("Estas autorizado?(si/no)")
+#autorizado = input() == "si"
 
 
-print("comparar si dos numeros enteros son iguales por medio de is: " + str(comparacion_dos_numeros_enteros))
+#Optimizando codigo
+nombre = input("Cual es tu nombre?\n")
+edad = int(input("Cual es tu edad?\n")) #de esta forma int va a tomar la funcion input() y la convertira en un entero
+peso = float(input("cual es tu peso?\n"))
+print("Estas autorizado?(si/no)")
+autorizado = input() == "si"
+
+
+print("Hola", nombre)
+print ("Edad", edad)
+print("Peso", peso)
+print("Autorizado", autorizado)
