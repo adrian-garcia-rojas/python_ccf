d3dd417b2ed342ec7f36211275b2998be24b2de4
adrian garcia rojas <adrian_gar016@hotmail.es>
2020-03-23 12:56:09 -0600

    Que son las listas?

--- Python_ccf.py
+++ Python_ccf.py
@@ -1,12 +1,12 @@
-#Comentarios
+#Que son las listas
 
-#Esta es una linea comentada
-"""
-Este es un comentario 
-con saltos de 
-lineas
-"""
+lista = []
 
+#nombre de la variable, signo igual, corchetes
 
-resultado = 20*20
-print(resultado)
+lista1 = ["Pepe", 23, True, 19.5]
+#La lista almacena string, numero entero, booleano y flotante
+#Con la listas se puede almacenar  N cantidad de estring, booleanos etc 
+# para saber el tamaño de la lista se debe considerar de la sig forma tam -1
+
+print(lista1)
