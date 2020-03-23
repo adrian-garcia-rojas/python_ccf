4075c219aec1f7aea9d96ccfbc911e69b70206c5
adrian garcia rojas <adrian_gar016@hotmail.es>
2020-03-23 11:39:12 -0600

    Asignacion multiple, se debe de usar con buenas practicas de legibilidad

--- Python_ccf.py
+++ Python_ccf.py
@@ -1,25 +1,5 @@
-#Palabras reservadas en Python
-#Para conocer el listado ingresar al siguiente link:
-#https://www.programiz.com/python-programming/keyword-list
+valor_uno, valor_dos, valor_tres = 10, "adrian garcia", 10*30
 
-
-
-"""Las palabras reservadas, como su nombre lo indican, son palabras las cuales el lenguaje de programación ya ha reservado para realizar ciertas tareas.
-
-    Nosotros no podemos declarar una variable con el mismo nombre que una palabra reservada
-
-En python el listado de palabras reservadas es el siguiente:
-			
-False 	class 	is 	return
-None 	continue 	lambda 	try
-True 	def 	nonlocal 	while
-and 	del 	not 	with
-as 	elif 	or 	yield
-assert 	else 	pass 	*
-break 	except 	raise 	*
-
-En el curso trabajaremos con la mayoría de las palabras reservadas, sin embargo, si quieres conocer todas ellas, puedes consultar el siguiente link
-"""
-
-enlace = "https://www.programiz.com/python-programming/keyword-list"
-print("ingresa a la siguinete direccion para conocer todas las palabras reservadas del lenguaje python y mucho mas hay una guia " + enlace)
+print(valor_uno)
+print(valor_dos)
+print(valor_tres)
