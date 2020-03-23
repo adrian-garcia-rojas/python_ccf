d28ce023bf63ec42e2f4c7369da285830138299e
adrian garcia rojas <adrian_gar016@hotmail.es>
2020-03-23 11:28:46 -0600

    Declaracion de variables

---
+++ Python_ccf.py
@@ -0,0 +1,5 @@
+CONSTANTE = "Soy una constante" #Las constantes no existen en python, se tiene que hacer esta declaracion con mayusculas asi otros programadores sabran que son constantes y no deben de modificar su valor
+nombre_tutor = "Adrian Garcia"
+
+print(nombre_tutor)
+print(CONSTANTE)
